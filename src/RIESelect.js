a68cfad6183bfa4a54db8956ad7cd327570aaa16
tspelde <tspelde@smartfile.com>
2018-06-26 17:34:02 -0400

    Allow string / number tolerance on <select> option value comparison

--- src/RIESelect.js
+++ src/RIESelect.js
@@ -11,12 +11,12 @@ export default class RIESelect extends RIEStatefulBase {
     finishEditing = () => {
         // get the object from options that matches user selected value
         const newValue = this.props.options.find(function(option) {
-            return option.id === ReactDOM.findDOMNode(this.refs.input).value;
+            return option.id == ReactDOM.findDOMNode(this.refs.input).value;
 		}, this);
 
 		this.doValidations(newValue);
 
-        if(!this.state.invalid && this.props.value !== newValue) {
+        if(!this.state.invalid && this.props.value != newValue) {
             this.commit(newValue);
 		}
 
